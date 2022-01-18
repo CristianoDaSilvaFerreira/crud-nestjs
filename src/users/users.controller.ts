a987d397a936bb7d6a40197e22d23021fc134420
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-01-18 20:29:49 -0300

    :rocket: Criando o usuário

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -12,7 +20,7 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
+  @Get('/profile')
   findAll() {
     return this.usersService.findAll();
   }
